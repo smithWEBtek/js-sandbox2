0f1e2916d70dbf966e5edde3d5f527e9df21ae87
github username <github email address>
2020-07-18 17:56:02 -0400

    Refactor components and App

--- react-router/rr1/src/App.js
+++ react-router/rr1/src/App.js
@@ -1,29 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Client from './Client';
-import Server from './Server';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+
 
 // Set up pages using the React Router Link element for navigation - instead of <a></a>
 const App = () => (
   <div>
-    <h1>React Router Example</h1>
-    <Router>
-      <ul>
-        <li><Link to="/client">Client Side</Link></li>
-        <li><Link to="/server">Server Side</Link></li>
-      </ul>
-      <Route path='/client' component={Client} />
-      <Route path='/server' component={Server} />
-    </Router>
+    <Header />
+    <NavBar />
   </div>
 )
 
 //Render app into the root HTML DOM node
 ReactDOM.render((
-  <Router>
-    <App />
-  </Router>
+  <App />
 ), document.getElementById('root'));
 
 export default App;
