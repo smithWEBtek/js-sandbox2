0ca8b52049b695f43788eb63839db38cfb3928f2
brad@smithwebtek.com <brad@smithwebtek.com>
2022-09-18 09:12:31 -0400

    Refactor select form to DropdownSelect component

--- mui-practice1/src/App.js
+++ mui-practice1/src/App.js
@@ -1,39 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import classes from './App.module.css';
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
+import DropdownSelect from './forms/DropdownSelect'
 
 function App() {
-
-  const [age, setAge] = useState(0)
-  const handleChange = (event) => {
-    setAge(event.target.value)
-    console.log('change: ', event.target.value);
-  }
-
   return (
     <div className={classes.App}>
       <Button variant="contained">Hello World</Button>
-      <div className={classes.FormControlContainer}>
-        <FormControl className={classes.FormControl}>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            className={classes.Select}
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            data-testid="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={0}></MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-          {age === 0 ? <FormHelperText>select</FormHelperText> : ''}
-        </FormControl>
-      </div >
+      <DropdownSelect
+        selections={['cat', 'bird', 'dog', 'fish']}
+      />
     </div >
   );
 }
